e2f30cb8628da5637486db33538c7507badc007b
Sondre Garnæs <sondre433@gmail.com>
2023-10-02 15:32:00 +0200

    Made it so you can move the pieces
    You can now move the pieces but it does not check yet if it is a legal move

--- chess_main.py
+++ chess_main.py
@@ -24,10 +24,28 @@ def main():
     gs=chess_engine.GameState()
     loadImages()
     running=True
+    sqSelected=() #no square is selected, keep track of the last click of the user (tuple:(row,col))
+    playerClicks=[] #keep track of player clicks (two tuples: [(6,4),(4,4)])
     while running:
         for e in p.event.get():
             if e.type==p.QUIT:
                 running=False
+            elif e.type==p.MOUSEBUTTONDOWN:
+                location=p.mouse.get_pos() #(x,y) location of mouse
+                col=location[0]//SQ_SIZE
+                row=location[1]//SQ_SIZE
+                if sqSelected==(row,col):
+                    sqSelected=() #deselect
+                    playerClicks=[] #clear player clicks
+                else:
+                    sqSelected=(row,col)
+                    playerClicks.append(sqSelected) #append for both 1st and 2nd clicks
+                if len(playerClicks)==2:
+                    move=chess_engine.Move(playerClicks[0],playerClicks[1],gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected=() #reset user clicks    
+                    playerClicks=[] 
         drawGameState(screen,gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -56,6 +74,8 @@ def drawPieces(screen,board):
 
 
 
+
 if __name__=="__main__":
     main()
+
 
