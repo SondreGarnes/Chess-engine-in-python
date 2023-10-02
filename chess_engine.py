a6592646abb1b81016abd2dd370e3042b099f848
Sondre Garnæs <sondre433@gmail.com>
2023-10-02 14:50:52 +0200

    Drew the board and put the pieces on the board

--- chess_engine.py
+++ chess_engine.py
@@ -1,3 +1,20 @@
 #This class is responsible for storing all the information about the current state of the chess game, 
 #also responsible for determining the valid moves at the current state. It will also keep a move log.
-import pygame as p
+class GameState():
+    def __init__(self):
+        #board is an 8x8 2d list, each element of the list has 2 characters.
+        #The first character represents the color of the piece, 'b' or 'w'
+        #The second character represents the type of the piece, 'K', 'Q', 'R', 'B', 'N' or 'P'
+        #"--" represents an empty space with no piece
+        self.board=[
+            ["bR","bN","bB","bQ","bK","bB","bN","bR"],
+            ["bP","bP","bP","bP","bP","bP","bP","bP"],
+            ["--","--","--","--","--","--","--","--"],
+            ['--','--','--','--','--','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
+            ["wP","wP","wP","wP","wP","wP","wP","wP"],
+            ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
+        self.whiteToMove=True
+        self.moveLog=[]
+        
--- chess_main.py
+++ chess_main.py
@@ -1,17 +1,61 @@
 #This is the main driver file, it will be responsible for handling user input and displaying the current GameState object
+import pygame as p
+import chess_engine
 
-class GameState():
-    def __init__(self):
-        #board is an 8x8 2d list, each element of the list has 2 characters.
-        #The first character represents the color of the piece, 'b' or 'w'
-        #The second character represents the type of the piece, 'K', 'Q', 'R', 'B', 'N' or 'P'
-        #"--" represents an empty space with no piece
-        self.board=[
-            ["bR","bN","bB","bQ","bK","bB","bN","bR"],
-            ["bp","bp","bp","bp","bp","bp","bp","bp"],
-            ["--","--","--","--","--","--","--","--"],
-            ['--','--','--','--','--','--','--','--'],
-            ['--','--','--','--','--','--','--','--'],
-            ['--','--','--','--','--','--','--','--'],
-            ["wp","wp","wp","wp","wp","wp","wp","wp"],
-            ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
+WIDTH=HEIGHT=512
+DIMENSION=8 #8x8 chess board
+SQ_SIZE=HEIGHT//DIMENSION
+MAX_FPS=15 #for animations later on
+IMAGES={}
+
+#Initialize a global dictionary of images. This will be called exactly once in the main
+def loadImages():
+    pieces=['wP','wR','wN','wB','wK','wQ','bP','bR','bN','bB','bK','bQ']
+    for piece in pieces:
+        IMAGES[piece]=p.transform.scale(p.image.load("Chess-engine-in-python/images/"+piece+".png"),(SQ_SIZE,SQ_SIZE))
+    #Note: we can access an image by saying 'IMAGES['wP']'
+
+#The main driver for our code. This will handle user input and updating the graphics
+def main():
+    p.init()
+    screen=p.display.set_mode((WIDTH,HEIGHT))
+    clock=p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs=chess_engine.GameState()
+    loadImages()
+    running=True
+    while running:
+        for e in p.event.get():
+            if e.type==p.QUIT:
+                running=False
+        drawGameState(screen,gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+#Responsible for all the graphics within a current game state
+def drawGameState(screen,gs):
+    drawBoard(screen) #draw squares on the board
+    drawPieces(screen,gs.board) #draw pieces on top of those squares
+
+#Draw the squares on the board.
+def drawBoard(screen):
+    colors=[p.Color("white"),p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color=colors[((r+c) % 2)]
+            p.draw.rect(screen,color,p.Rect(c*SQ_SIZE,r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+
+#Draw the pieces on the board using the current GameState.board
+def drawPieces(screen,board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece=board[r][c]
+            if piece!="--":
+                screen.blit(IMAGES[piece],p.Rect(c*SQ_SIZE,r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+
+
+
+
+if __name__=="__main__":
+    main()
+
