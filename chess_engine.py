a7357306cbbee70a406f0823f0c637bd7913ae35
Sondre Garnæs <sondre433@gmail.com>
2023-10-04 20:14:13 +0200

    Check, checkmate and stalemate

--- chess_engine.py
+++ chess_engine.py
@@ -19,6 +19,12 @@ class GameState():
                             "B":self.getBishopMoves,"Q":self.getQueenMoves,"K":self.getKingMoves}
         self.whiteToMove=True
         self.moveLog=[]
+        self.whiteKingLocation=(7,4)
+        self.blackKingLocation=(0,4)
+        self.chechMate=False
+        self.staleMate=False
+
+
 
 
 
@@ -27,6 +33,11 @@ class GameState():
         self.board[move.endRow][move.endCol]=move.pieceMoved
         self.moveLog.append(move) #log the move so we can undo it later
         self.whiteToMove=not self.whiteToMove #swap players
+        #update the king's location if moved
+        if move.pieceMoved=="wK":
+            self.whiteKingLocation=(move.endRow,move.endCol)
+        elif move.pieceMoved=="bK":
+            self.blackKingLocation=(move.endRow,move.endCol)
 
     def undoMove(self):
         if len(self.moveLog)!=0: #if there is a move to undo
@@ -34,10 +45,55 @@ class GameState():
             self.board[move.startRow][move.startCol]=move.pieceMoved
             self.board[move.endRow][move.endCol]=move.pieceCaptured
             self.whiteToMove=not self.whiteToMove
+            #update the king's location if moved
+            if move.pieceMoved=="wK":
+                self.whiteKingLocation=(move.startRow,move.startCol)
+            elif move.pieceMoved=="bK":
+                self.blackKingLocation=(move.startRow,move.startCol)
+
     
     def getValidMoves(self):
-        return self.getAllPossibleMoves()
+        #1)generate all possible moves
+        moves=self.getAllPossibleMoves()
+
+        #2)for each move, make the move
+        for i in range(len(moves)-1,-1,-1): #when removing from a list, go backwards through that list
+            self.makeMove(moves[i])
+
+            self.whiteToMove=not self.whiteToMove #switch turns
+            if self.inCheck():
+                moves.remove(moves[i])#if the move leaves you in check, it's not a valid move
+            self.whiteToMove=not self.whiteToMove #switch turns back
+            self.undoMove()
+        if len(moves)==0:
+            if self.inCheck():
+                self.checkMate=True
+            else:
+                self.staleMate=True
+        else:
+            self.checkMate=False
+            self.staleMate=False
+        return moves
+        
+
+        
     
+    def inCheck(self):
+        if self.whiteToMove:
+            return self.squareUnderAttack(self.whiteKingLocation[0],self.whiteKingLocation[1])
+        else:
+            return self.squareUnderAttack(self.blackKingLocation[0],self.blackKingLocation[1])
+
+    def squareUnderAttack(self,r,c):
+        self.whiteToMove=not self.whiteToMove #switch to opponent's turn
+        oppMoves=self.getAllPossibleMoves()
+        self.whiteToMove=not self.whiteToMove #switch turns back
+        for move in oppMoves:
+            if move.endRow==r and move.endCol==c:
+                return True
+        return False
+    
+
     def getAllPossibleMoves(self):
         moves = []
         for r in range(len(self.board)):
